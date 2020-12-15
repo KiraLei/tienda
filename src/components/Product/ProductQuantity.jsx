6834e0bba655d4dc748e94ab0bf4c1123a4e05a8
Tata.Quispe Sarmiento <mquispes@proveedor.intercorp.com.pe>
2020-12-15 00:13:59 -0500

    finished contador con boton

--- src/components/Product/ProductQuantity.jsx
+++ src/components/Product/ProductQuantity.jsx
@@ -1,11 +1,14 @@
+import {useState} from 'react';
 import '../../styles/ProductQuantity.scss'
 function ProductQuantity(){
+   /* const [nombre, setNombre] =  useState('');*/
+    const [quantity, setQuantity] =  useState(1);
 
     return(
         <div className="product-quantity">
-            <button>-</button>
-            <input type="text" value="1" />
-            <button>+</button>
+            <button className="disable" onClick= {()=>{setQuantity(quantity - 1 )}} disabled={quantity <= 0}>-</button>
+            <input type="text" value={quantity} disabled />
+            <button onClick={()=>{setQuantity( quantity + 1)}}>+</button>   
         </div>
     )
 }
